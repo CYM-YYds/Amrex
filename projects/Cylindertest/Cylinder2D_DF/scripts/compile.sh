15a611b00cae45d8ab466ab4094ef7aa48150a84
蔡以民 <2401473791@qq.com>
2025-12-25 16:33:59 +0800

    加入了AmreX源代码

--- projects/Cylindertest/Cylinder2D_DF/scripts/compile.sh
+++ projects/Cylindertest/Cylinder2D_DF/scripts/compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# 代理脚本：转发到 Cylindertest/scripts/compile.sh
 set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-SHARED_DIR="${SCRIPT_DIR}/../scripts"
-exec "${SHARED_DIR}/compile.sh" "$@"
+SHARED_DIR="${SCRIPT_DIR}/../../scripts"
+#exec "${SHARED_DIR}/compile.sh" "$@"
+exec "/home/huazkjdxmrsgjzdsyshi/whcs-share18/caiyimin/learnamerx/Amrex/scripts/compile.sh" "$@"
